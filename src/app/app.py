46ce491f9a19db7f2ee1c8d786b43c21edbecd22
ProtossDragoon <dlwkdgn1@naver.com>
2022-05-26 19:27:08 +0900

    bugfix: requirements 수정

--- src/app/app.py
+++ src/app/app.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.write('6:59')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
@@ -16,11 +17,8 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
@@ -31,10 +29,10 @@ st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-st.subheader('Map of all pickups')
-st.map(data)
-
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
